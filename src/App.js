480ff921ac456f7d4fe8e5209e85525d8f2efafd
Will Murphy <william@datatitian.com>
2022-03-08 22:07:51 -0600

    scrolly path through gltf waypoints

--- src/App.js
+++ src/App.js
@@ -1,59 +1,98 @@
 // import * as THREE from 'three'
-import { Suspense, useLayoutEffect } from 'react'
+import { Suspense, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { ScrollControls, Sky, useScroll, useGLTF } from '@react-three/drei'
 
-import apartment from './assets/Apartment 1.glb'
-import roof from "./assets/Apartment 1 roof.glb"
+import apartment from './assets/Immers Home.glb'
+import { CatmullRomCurve3, Vector3 } from 'three'
+
+const alphaName = (a, b) => {
+  if (a.name < b.name) {
+    return -1
+  }
+  if (a.name > b.name) {
+    return 1
+  }
+  return 0
+}
 
 export default function App() {
+  const [waypoints, setWaypoints] = useState([]);
+
   return (
-    <Canvas dpr={[1, 2]} shadows camera={{ position: [0, 0, 10], near: 0.1, far: 1000 }}>
-      <ambientLight intensity={0.3} />
-      {/* <fog attach="fog" args={['#ffffff', 5, 18]} /> */}
+    <Canvas dpr={[1, 2]} shadows camera={{ position: [10, 10, -10], near: 0.1, far: 1000 }}>
+      <ambientLight intensity={0.5} />
       <spotLight angle={0.14} color="#ffffff" penumbra={1} position={[25, 50, -20]} shadow-mapSize={[2048, 2048]} shadow-bias={-0.0001} castShadow />
       <Sky scale={1000} sunPosition={[2, 0.4, 10]} />
       <Suspense fallback={null}>
-        {/* Wrap contents you want to scroll into <ScrollControls> */}
-        <ScrollControls pages={3}>
-          <mesh rotation={[0, 3.14, 0]}>
-            <Apartment scale={1} position={[0, 0, 0]} />
-            <Roof scale={1} position={[0, 0, 0]} />
-          </mesh>
+        <ScrollControls pages={waypoints.length}>
+          <group>
+            <Apartment setWaypoints={setWaypoints} scale={1} position={[0, 0, 0]} />
+            <WaypointPath waypoints={waypoints} height={1.2} />
+          </group>
         </ScrollControls>
       </Suspense>
     </Canvas>
   )
 }
 
-function Apartment({ ...props }) {
-  // This hook gives you offets, ranges and other useful things
-  const scroll = useScroll()
+function Apartment({ setWaypoints, hideRoof, ...props }) {
   const { scene, nodes } = useGLTF(apartment)
-  // const { actions } = useAnimations(animations, scene)
-  useLayoutEffect(() => Object.values(nodes).forEach((node) => (node.receiveShadow = node.castShadow = true)))
-  // useEffect(() => void (actions['Take 001'].play().paused = true), [actions])
-  useFrame((state, delta) => {
-    // const action = actions['Take 001']
-    // The offset is between 0 and 1, you can apply it to your models any way you like
-    const offset = 1 - scroll.offset
-    // action.time = THREE.MathUtils.damp(action.time, (action.getClip().duration / 2) * offset, 100, delta)
-    state.camera.position.set(Math.sin(offset) * -10, Math.atan(offset * Math.PI * 2) * 5, Math.cos((offset * Math.PI) / 3) * -10)
-    state.camera.lookAt(0, 0, 0)
-  })
-  return <primitive object={scene} {...props} />
+  const roof = useRef();
+  useLayoutEffect(() => {
+    const waypointNodes = []
+    Object.values(nodes).forEach((node) => {
+      // node.receiveShadow = node.castShadow = true
+      if (node.userData?.gltfExtensions?.MOZ_hubs_components?.waypoint) {
+        waypointNodes.push(node)
+      }
+      if (node.name === 'roof') {
+        roof.current = node
+        roof.current.visible = !hideRoof
+      }
+    })
+    waypointNodes.sort(alphaName)
+    setWaypoints(waypointNodes)
+  }, [nodes, setWaypoints, hideRoof])
+
+  return (
+    <group>
+      <primitive object={scene} {...props} />
+    </group>
+  )
 }
 
-function Roof({...props}) {
-  const { scene, nodes } = useGLTF(roof)
-  useLayoutEffect(() => Object.values(nodes).forEach((node) => {
-    if (node.material) {
-      node.material.transparent = true
-      node.material.opacity = 0.25
+function WaypointPath({waypoints, height, ...props}) {
+  const geo = useRef()
+  const curve = useRef()
+  const target = useRef(new Vector3())
+  const scroll = useScroll()
+
+  useEffect(() => {
+    if (!waypoints.length) {
+      return
     }
-  }))
-  return <primitive object={scene} {...props} />
+    const points = waypoints.map(w => w.position)
+    curve.current = new CatmullRomCurve3(points, false)
+    geo.current.setFromPoints(curve.current.getPoints(50))
+  }, [waypoints])
+  useFrame((state, delta) => {
+    if (!curve.current) {
+      return
+    }
+    const t = scroll.offset
+    curve.current.getPointAt(t, state.camera.position)
+    state.camera.position.y += height
+    curve.current.getTangentAt(t, target.current)
+    target.current.add(state.camera.position)
+    state.camera.lookAt(target.current)
+  })
+  return (
+    <line position={[0, 0.2, 0]} frustumCulled={false} {...props}>
+      <bufferGeometry ref={geo} />
+      <lineBasicMaterial color='red' />
+    </line>
+  )
 }
 
 useGLTF.preload(apartment)
-useGLTF.preload(roof)
