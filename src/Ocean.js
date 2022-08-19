e1e236a3d79d3c2b1243196a3b754bd818ba38e1
Will Murphy <william@datatitian.com>
2022-08-19 13:26:35 -0500

    animate water level

--- src/Ocean.js
+++ src/Ocean.js
@@ -32,6 +32,9 @@ export default function Ocean({ position, sunPosition }) {
       ref.current.material.transparent = true
     }
   }, [ref])
-  useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta / 7.5))
+  useFrame((state, delta) => {
+    ref.current.material.uniforms.time.value += delta / 7.5
+    ref.current.position.y = position[1] + Math.sin(state.clock.elapsedTime) / 20
+  })
   return <water ref={ref} args={[geom, config]} position={position} rotation-x={-Math.PI / 2} />
 }
